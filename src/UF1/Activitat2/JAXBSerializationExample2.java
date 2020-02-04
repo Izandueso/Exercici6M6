343a9861e88b3e33bdb1b5f546fcec6bfe0b0afe
Alumne <Alumne@104-PC11>
2020-02-04 20:57:42 +0100

    LastCommit

--- src/UF1/Activitat2/JAXBSerializationExample2.java
+++ src/UF1/Activitat2/JAXBSerializationExample2.java
@@ -21,11 +21,11 @@ public class JAXBSerializationExample2 {
  
 	public static void main(String[] args) throws JAXBException, IOException {
 		
-		JAXBContext context = JAXBContext.newInstance(Alumnes.class);
+		JAXBContext context = JAXBContext.newInstance(Rows.class);
 		Marshaller marshaller = context.createMarshaller();
 		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 		
-		Alumnes rows = ompleRows();
+		Rows rows = ompleRows();
 		
 		//Mostrem el document XML generat por la sortida estandard
 		marshaller.marshal(rows, System.out);
@@ -37,20 +37,29 @@ public class JAXBSerializationExample2 {
 		
 		Unmarshaller unmarshaller = context.createUnmarshaller();
 		//Deserialitzem a partir de un document XML
-		Alumnes alumnesAux = (Alumnes) unmarshaller.unmarshal(new File(ROWS_XML_FILE));
-		System.out.println("********* Alumnes carregat desde fitxer XML***************");
+		Rows alumnesAux = (Rows) unmarshaller.unmarshal(new File(ROWS_XML_FILE));
+		System.out.println("********* Rows carregats desde fitxer XML***************");
 		//Mostrem l'objeto Java obtingut
 		marshaller.marshal(alumnesAux, System.out);
 		
 	}
 	
-	private static Alumnes ompleRows(){
+	private static Rows ompleRows(){
 		
 		
 		String[] nomDelFestiu = {"Fiesta1", "Fiesta2", "Fiesta3", "Fiesta4"};
-        String[] data = {"2018-01-06T00:00:00", "2018-01-06T00:00:00", "2018-01-06T00:00:00", "2018-01-06T00:00:00"};
+        String[] data = {"2018-01-06T00:04:00", "2018-01-06T00:10:00", "2018-01-06T00:20:00", "2018-01-06T00:03:00"};
         String[] localitzacio = {"Salou", "Tarragona", "Reus", "Valls"};
 		int[] any_calendari = {2016, 1820, 1999, 2020};
+		String[] _adress = {"https://analisfi.transparenciacatalunya.cat/resource/yf2b-mjr6/row-efjc~t6wi.7y5k",
+				"https://analisi.transparencgiacatalunya.cat/resource/yf2b-mjr6/row-efjc~t6wi.7y5k",
+				"https://analisi.transdparenciacatalunya.cat/resource/yf2b-mjr6/row-efjc~t6wi.7y5k",
+				"https://analisi.transparencigacatalunya.cat/resource/yf2b-mjr6/row-efjc~t6wi.7y5k"};
+		String[] _id = {"row-efjc~t6wi.7y5k","row-efjc~t6wi.7y5k",
+				"row-efjc~t6wi.7y5k","row-efjc~t6wi.7y5k"};
+		int[] positions = {1,4,8,3};
+		String[] _uuid = {"00000000-0000-0000-27CA-66ABEA857B6F","00000000-0000-0000-27CA-66ABEA857B6F",
+				"00000000-0000-0000-27CA-66ABEA857B6F","00000000-0000-0000-27CA-66ABEA857B6F"};
 		Alumne[] ArrayAlumnes = new Alumne[4];
 		
 		for(int i=0; i<4; i++){
@@ -59,13 +68,16 @@ public class JAXBSerializationExample2 {
                         ArrayAlumnes[i].setData(data[i]);
                         ArrayAlumnes[i].setLocalitzacio(localitzacio[i]);
                         ArrayAlumnes[i].setAnyCalendari(any_calendari[i]);
-			ArrayAlumnes[i].setId(i);			
+			ArrayAlumnes[i].setId(i);	
+			ArrayAlumnes[i].setAdress(_adress[i]);
+			ArrayAlumnes[i].setPosition(positions[i]);
+			ArrayAlumnes[i].setUuid(_uuid[i]);
 		}
 		
-		Alumnes alumnes = new Alumnes();
-		alumnes.setAlumnes(ArrayAlumnes);
+		Rows rows = new Rows();
+		rows.setAlumnes(ArrayAlumnes);
 		
-		return alumnes;
+		return rows;
 	}
  
 }
